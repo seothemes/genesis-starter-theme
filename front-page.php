dc2b15607cb24e77828d3c5bd61127a3d8de4f77
seothemes <seothemeswp@gmail.com>
2017-09-10 14:06:16 +0800

    Avoid hijacking the genesis flow on front page template

--- front-page.php
+++ front-page.php
@@ -10,64 +10,63 @@
  * @link    https://seothemes.com/themes/genesis-starter/
  */
 
-// If any widget areas are active, do custom front page.
-if ( is_active_sidebar( 'front-page-1' ) || is_active_sidebar( 'front-page-2' ) || is_active_sidebar( 'front-page-3' ) || is_active_sidebar( 'front-page-4' ) || is_active_sidebar( 'front-page-5' ) || is_active_sidebar( 'front-page-6' ) ) {
+add_action( 'genesis_after_header', 'starter_front_page', 19 );
+/**
+ * Front page content.
+ *
+ * @return void
+ */
+function starter_front_page() {
 
-	/**
-	 * Add attributes for site-inner element.
-	 *
-	 * @since 2.0.0
-	 *
-	 * @param array $attributes Existing attributes.
-	 *
-	 * @return array Amended attributes.
-	 */
-	function starter_attributes_site_inner( $attributes ) {
-		$attributes['class']    = 'front-page-widgets';
-		$attributes['role']     = 'main';
-		$attributes['itemprop'] = 'mainContentOfPage';
-		return $attributes;
+	// Check if any front page widgets are active.
+	if ( is_active_sidebar( 'front-page-1' ) || is_active_sidebar( 'front-page-2' ) || is_active_sidebar( 'front-page-3' ) || is_active_sidebar( 'front-page-4' ) || is_active_sidebar( 'front-page-5' ) ) {
+
+		// Remove default page header.
+		remove_action( 'genesis_after_header', 'starter_header_open', 20 );
+		remove_action( 'genesis_after_header', 'starter_header_title', 24 );
+		remove_action( 'genesis_after_header', 'starter_header_close', 28 );
+
+		// Remove site inner content.
+		remove_action( 'genesis_loop', 'genesis_do_loop' );
+		remove_action( 'genesis_after_content', 'genesis_get_sidebar' );
+
+		// Remove site inner markup.
+		add_filter( 'genesis_markup_site-inner', '__return_null' );
+		add_filter( 'genesis_markup_content-sidebar-wrap', '__return_null' );
+		add_filter( 'genesis_markup_content', '__return_null' );
+
+		// Front page 1 widget area.
+		genesis_widget_area( 'front-page-1', array(
+			'before' => '<div class="front-page-1 page-header" role="banner"><div class="wrap">',
+			'after'  => '</div></div>',
+		) );
+
+		// Front page 2 widget area.
+		genesis_widget_area( 'front-page-2', array(
+			'before' => '<div class="front-page-2 widget-area"><div class="wrap">',
+			'after'  => '</div></div>',
+		) );
+
+		// Front page 3 widget area.
+		genesis_widget_area( 'front-page-3', array(
+			'before' => '<div class="front-page-3 widget-area"><div class="wrap">',
+			'after'  => '</div></div>',
+		) );
+
+		// Front page 4 widget area.
+		genesis_widget_area( 'front-page-4', array(
+			'before' => '<div class="front-page-4 widget-area"><div class="wrap">',
+			'after'  => '</div></div>',
+		) );
+
+		// Front page 5 widget area.
+		genesis_widget_area( 'front-page-5', array(
+			'before' => '<div class="front-page-5 widget-area"><div class="wrap">',
+			'after'  => '</div></div>',
+		) );
+
 	}
-	add_filter( 'genesis_attr_site-inner', 'starter_attributes_site_inner' );
+}
 
-	// Remove site-inner wrap.
-	add_filter( 'genesis_structural_wrap-site-inner', '__return_empty_string' );
-
-	// Get header.
-	get_header();
-
-	// Display widget areas.
-	genesis_widget_area( 'front-page-1', array(
-		'before' => '<div class="front-page-1 widget-area"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-	genesis_widget_area( 'front-page-2', array(
-		'before' => '<div class="front-page-2 widget-area"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-	genesis_widget_area( 'front-page-3', array(
-		'before' => '<div class="front-page-3 widget-area"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-	genesis_widget_area( 'front-page-4', array(
-		'before' => '<div class="front-page-4 widget-area"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-	genesis_widget_area( 'front-page-5', array(
-		'before' => '<div class="front-page-5 widget-area"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-	genesis_widget_area( 'front-page-6', array(
-		'before' => '<div class="front-page-6 widget-area"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-
-	// Get footer.
-	get_footer();
-
-} else {
-
-	// Do the default loop.
-	genesis();
-
-} // End if().
+// Run Genesis.
+genesis();
