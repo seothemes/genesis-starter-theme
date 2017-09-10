31f88b54df2706f1987e5bae6b744cf53a5125a1
seothemes <seothemeswp@gmail.com>
2017-09-10 14:05:46 +0800

    Add custom 404 template

---
+++ 404.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Genesis Starter.
+ *
+ * This file adds the 404 page template to the Genesis Starter Theme.
+ *
+ * @package Genesis Starter
+ * @author  SeoThemes
+ * @license GPL-2.0+
+ * @link    https://seothemes.com/themes/genesis-starter/
+ */
+
+// Remove default loop.
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+add_action( 'genesis_loop', 'starter_404' );
+/**
+ * This function outputs a 404 "Not Found" error message.
+ *
+ * @since 1.6
+ */
+function starter_404() {
+
+	genesis_markup( array(
+		'open' => '<article class="entry">',
+		'context' => 'entry-404',
+	) );
+
+		/* translators: %s: URL for current website. */
+		echo apply_filters( 'genesis_404_entry_content', '<p>' . sprintf( __( 'The page you are looking for no longer exists. Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it by using the search form below.', 'genesis' ), trailingslashit( home_url() ) ) . '</p>' );
+
+		get_search_form();
+
+		echo '<h2>' . __( 'Sitemap', 'genesis-starter' ) . '</h2>';
+
+		genesis_sitemap( 'h3' );
+
+	genesis_markup( array(
+		'close' => '</article>',
+		'context' => 'entry-404',
+	) );
+
+}
+
+genesis();
