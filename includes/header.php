9b2789dabc01d91c3337504cbfa25c1f004c7b02
seothemes <seothemeswp@gmail.com>
2017-09-10 14:24:02 +0800

    Fix page header bugs

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 }
 
-// Remove titles.
+// Remove default titles.
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
@@ -30,7 +30,7 @@ remove_action( 'genesis_before_loop', 'genesis_do_author_title_description', 15 
 remove_action( 'genesis_before_loop', 'genesis_do_cpt_archive_title_description' );
 remove_action( 'genesis_before_loop', 'genesis_do_search_title' );
 
-// Hooks.
+// Add custom titles.
 add_action( 'genesis_after_header', 'starter_page_header_open', 20 );
 add_action( 'genesis_after_header', 'starter_page_header_title', 24 );
 add_action( 'genesis_after_header', 'genesis_do_posts_page_heading', 24 );
@@ -41,8 +41,9 @@ add_action( 'genesis_after_header', 'genesis_do_author_title_description', 24 );
 add_action( 'genesis_after_header', 'genesis_do_cpt_archive_title_description', 24 );
 add_action( 'genesis_after_header', 'starter_page_header_close', 28 );
 
-// Hide WooCommerce Page Title.
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+// Remove search results and shop page titles.
+add_filter( 'woocommerce_show_page_title', '__return_null' );
+add_filter( 'genesis_search_title_output', '__return_false' );
 
 /**
  * Opening markup.
@@ -79,36 +80,38 @@ function starter_page_header_close() {
  */
 function starter_page_header_title() {
 
-	if ( class_exists( 'WooCommerce' ) && is_shop() ) {
-
-		echo get_the_title( get_option( 'woocommerce_shop_page_id' ) );
-		echo esc_html( strip_tags( get_the_excerpt( get_option( 'woocommerce_shop_page_id' ) ) ) );
-
-	} elseif ( 'posts' === get_option( 'show_on_front' ) && is_home() ) {
-
-		printf( '<h1>%s</h1>', esc_html( apply_filters( 'starter_latest_posts_title', __( 'Latest Posts', 'genesis-starter' ) ) ) );
-		printf( '<p>%s</p>', esc_html( apply_filters( 'starter_latest_posts_subtitle', __( 'Showing the latest posts', 'genesis-starter' ) ) ) );
-
-	} elseif ( is_search() ) {
-
-		genesis_do_search_title();
-
-	} elseif ( is_404() ) {
-
-		printf( '<h1 class="entry-title">%s</h1>', esc_html( apply_filters( 'genesis_404_entry_title', __( 'Not found, error 404', 'genesis' ) ) ) );
-
-	} elseif ( is_single() || is_singular() ) {
-
-		the_title( '<h1 itemprop="headline">', '</h1>', true );
-		has_excerpt() ? printf( '<p itemprop="description">%s</p>', esc_html( strip_tags( get_the_excerpt() ) ) ) : '';
-
-	} // End if().
-
 	// Add post titles back inside posts loop.
 	if ( is_home() || is_archive() || is_category() || is_tag() || is_tax() || is_search() || is_page_template( 'page_blog.php' ) ) {
 
 		add_action( 'genesis_entry_header', 'genesis_do_post_title', 2 );
 
+	}
+
+	if ( class_exists( 'WooCommerce' ) && is_shop() ) {
+
+		printf( '<h1 itemprop="headline">%s</h1>', get_the_title( get_option( 'woocommerce_shop_page_id' ) ) );
+
+	} elseif ( 'posts' === get_option( 'show_on_front' ) && is_home() ) {
+
+		printf( '<h1 itemprop="headline">%s</h1>', esc_html( apply_filters( 'starter_latest_posts_title', __( 'Latest Posts', 'genesis-starter' ) ) ) );
+
+	} elseif ( is_404() ) {
+
+		printf( '<h1 itemprop="headline">%s</h1>', esc_html( apply_filters( 'genesis_404_entry_title', __( 'Not found, error 404', 'genesis-starter' ) ) ) );
+
+	} elseif ( is_search() ) {
+
+		printf( '<h1 itemprop="headline">%s %s</h1></div>', apply_filters( 'genesis_search_title_text', __( 'Search Results for:', 'genesis-starter' ) ), get_search_query() );
+
+	} elseif ( is_single() || is_singular() ) {
+
+		the_title( '<h1 itemprop="headline">', '</h1>', true );
+
+		if ( has_excerpt() ) {
+
+			printf( '<p itemprop="description">%s</p>', esc_html( strip_tags( get_the_excerpt() ) ) );
+
+		}
 	}
 
 }
