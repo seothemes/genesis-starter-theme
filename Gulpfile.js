a85c55ebae82afabc359001c0cedfa253892d4ed
seothemes <seothemeswp@gmail.com>
2018-08-15 17:47:36 +0800

    Use HTTP by default for BrowserSync

--- Gulpfile.js
+++ Gulpfile.js
@@ -75,14 +75,14 @@ toolkit.extendConfig(
 			js: './resources/js/'
 		},
 		server: {
-            proxy: 'https://genesis-starter.test',
+            proxy: 'http://genesis-starter.test',
 			host: 'genesis-starter.test',
 			open: 'external',
-            port: '8000',
-            https: {
-            	'key': '/Users/seothemes/.valet/Certificates/genesis-starter.test.key',
-            	'cert': '/Users/seothemes/.valet/Certificates/genesis-starter.test.crt'
-            }
+            port: '8000'
+            // https: {
+            // 	   'key': '/Users/seothemes/.valet/Certificates/genesis-starter.test.key',
+            // 	   'cert': '/Users/seothemes/.valet/Certificates/genesis-starter.test.crt'
+            // }
 		}
 	}
 );
